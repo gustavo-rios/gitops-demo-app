2937e9e0b9c2550f9809266611b74d5ab31b4d44
Gustavo Rios <garios@Gustavos-MacBook-Pro-2.local>
2020-08-29 08:45:49 -0400

    Changed headline

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var server = http.createServer(function(request, response) {
   response.write("<html>");
   response.write("<head><title>GitOps On Amazon EKS</title><style>body { background-color: blue; }</style></head>");
   response.write("<body>");
-  response.write("<h1 style=\"text-align:center\">GitOps on EKS is AWSome!!</h1>");
+  response.write("<h1 style=\"text-align:center\">Welcome to GitOps on EKS!!</h1>");
   response.write("</body>");
   response.write("</html>");
   response.end();
